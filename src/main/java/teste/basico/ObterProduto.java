afe4f15c9ec2f73e03b0a8737dc616880dabcd02
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-03-12 11:54:41 -0300

    feat: Criando classe para obter produtos e soma dos valores

---
+++ src/main/java/teste/basico/ObterProduto.java
@@ -0,0 +1,28 @@
+package teste.basico;
+
+import java.util.List;
+
+import infra.ProdutoDAO;
+import modelo.basico.Produto;
+
+public class ObterProduto {
+
+	public static void main(String[] args) {
+		
+		ProdutoDAO dao = new ProdutoDAO();
+		List<Produto> produtos = dao.obterTodos();
+		
+		for(Produto produto: produtos) {
+			System.out.println("Id: " + produto.getId() 
+			+ " \nnome: " + produto.getNome());
+		}
+		
+		double precoTotal = produtos
+				.stream()
+				.map(p -> p.getPreco())
+				.reduce(0.0, (t, p) -> t+p)
+				.doubleValue();
+		System.out.println("Valor total é: " + precoTotal);
+	}
+	
+}
