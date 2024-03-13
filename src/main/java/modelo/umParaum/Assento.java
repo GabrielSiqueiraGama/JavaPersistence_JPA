ac3785a3babbb7c6dd27a9d83fc55f12dafd8ff2
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-03-13 11:42:26 -0300

    feat: Criando classes para entidades de Um para Um

---
+++ src/main/java/modelo/umParaum/Assento.java
@@ -0,0 +1,19 @@
+package modelo.umParaum;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "assentos")
+public class Assento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+	
+}
---
+++ src/main/java/modelo/umParaum/Cliente.java
@@ -0,0 +1,20 @@
+package modelo.umParaum;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String name;
+	
+	
+}
