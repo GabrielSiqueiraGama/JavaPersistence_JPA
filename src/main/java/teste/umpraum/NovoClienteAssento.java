dd2d70cdf36d1035ccdc6cabd228fa9548681e6b
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-03-16 23:28:24 -0300

    feat: Adding more Clients

--- src/main/java/teste/umpraum/NovoClienteAssento.java
+++ src/main/java/teste/umpraum/NovoClienteAssento.java
@@ -10,12 +10,25 @@ public class NovoClienteAssento {
 		
 		DAO<Object> dao = new DAO<>();
 		
-		Assento assento = new Assento("1A");
-		Cliente cliente = new Cliente("Carlos", assento);
+		Assento assento1 = new Assento("1A");
+		Assento assento2 = new Assento("2A");
+		Assento assento3 = new Assento("3A");
+		Assento assento4 = new Assento("4A");
+		
+		Cliente cliente1 = new Cliente("Carlos", assento1);
+		Cliente cliente2 = new Cliente("Adoninas", assento2);
+		Cliente cliente3 = new Cliente("Peléz", assento3);
+		Cliente cliente4 = new Cliente("Ronallinho", assento4);
 		
 		dao.abrirT()
-			.incluir(assento)
-			.incluir(cliente)
+			.incluir(assento1)
+			.incluir(assento2)
+			.incluir(assento3)
+			.incluir(assento4)
+			.incluir(cliente1)
+			.incluir(cliente2)
+			.incluir(cliente3)
+			.incluir(cliente4)
 				.fecharT()
 					.fechar();
 		
