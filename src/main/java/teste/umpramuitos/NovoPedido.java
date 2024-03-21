7087ca86cd32492a5901776867140a056ce461c3
GabrielSiqueiraGama <siqueiragamagabriel@gmail.com>
2024-03-20 23:12:51 -0300

    feat: Adicionando em relação um para muitos

--- src/main/java/teste/umpramuitos/NovoPedido.java
+++ src/main/java/teste/umpramuitos/NovoPedido.java
@@ -1,14 +1,26 @@
 package teste.umpramuitos;
 
+import infra.DAO;
 import modelo.basico.Produto;
+import modelo.umParamuitos.ItemPedido;
 import modelo.umParamuitos.Pedido;
 
 public class NovoPedido {
 
 	public static void main(String[] args) {
 		
+		Pedido pedido = new Pedido();
 		Produto produto = new Produto("Geladeira inox", 2999.99);
-		Pedido ped1do = new Pedido();
+		ItemPedido item = new ItemPedido(pedido, produto, 10);
+		
+		DAO<Object> dao = new DAO<>();
+		
+		dao.abrirT()
+			.incluir(produto)
+			.incluir(pedido)
+			.incluir(item)
+			.fecharT()
+			.fechar();
 	}
 	
 }
